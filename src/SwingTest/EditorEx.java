342b1eec12cdf260dc09c373ed1fadb25ef9f6d5
dlacked <imcy0104@gmail.com>
2025-11-19 22:29:49 +0900

    댓글 기능 추가 및 점 찍기 시도 시 안 찍히는 버그 수정

--- src/SwingTest/EditorEx.java
+++ src/SwingTest/EditorEx.java
@@ -50,8 +50,8 @@ public class EditorEx extends JFrame {
 		JMenuItem redo = new JMenuItem("Redo");
 		JMenuItem erase_all = new JMenuItem("Erase All");
 
-		penOption.add(colorSelect);
 		penOption.add(widthInput);
+		penOption.add(colorSelect);
 		
 		eraserOption.add(undo);
 		eraserOption.add(redo);
@@ -71,8 +71,8 @@ public class EditorEx extends JFrame {
             		drawPane.splineList.remove(drawPane.splineList.size()-1);
                 	drawPane.repaint();
             	}
-            	System.out.println("splineList:" + drawPane.splineList.size());
-            	System.out.println("erasedSplineList:" + drawPane.erasedSplineList.size());
+//            	System.out.println("splineList:" + drawPane.splineList.size());
+//            	System.out.println("erasedSplineList:" + drawPane.erasedSplineList.size());
             	
             }
         });
@@ -132,29 +132,68 @@ public class EditorEx extends JFrame {
 				EditorEx.this.penWidth = Integer.parseInt(inputWidth);
 			}
 		});
+		
+		
         
-        
-        
+        //layout
         Container c = getContentPane();
         
         c.setLayout(new BorderLayout());
 
         drawPane = new DrawingPanel();
-        drawPane.setBackground(Color.WHITE);
-        c.add(drawPane, BorderLayout.CENTER);
         
-               
+        //설레는 댓글창
+        JTextArea  consolePane = new JTextArea();
+        consolePane.setLineWrap(true);        // 자동 줄바꿈
+        consolePane.setWrapStyleWord(true);
+        JScrollPane consoleScrollPane = new JScrollPane(consolePane);
+        consoleScrollPane.setPreferredSize(new Dimension(200, 100));
+        consoleScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        c.add(consoleScrollPane, BorderLayout.SOUTH); 
+        
         DefaultListModel<String> model = new DefaultListModel<>();
-        model.addElement("hello.java");
-        model.addElement("world.java");
         
 		JList<String> list = new JList<>(model);
+		list.setEnabled(false);
+		list.setLayoutOrientation(JList.VERTICAL);
+		list.setFixedCellWidth(150);  
+		model.addElement("***************************************");
+		model.addElement("Admin: 댓글을 입력해주세요.");
+		model.addElement("***************************************");
 		
 		JScrollPane listScrollPane = new JScrollPane(list);
-		listScrollPane.setPreferredSize(new Dimension(150, 100));
+		listScrollPane.setPreferredSize(new Dimension(200, 100));
+        listScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		
 		c.add(listScrollPane, BorderLayout.EAST);
+		
+		JPanel rightPanel = new JPanel(new BorderLayout());
+		rightPanel.add(listScrollPane, BorderLayout.CENTER);
+		rightPanel.add(consoleScrollPane, BorderLayout.SOUTH);
 
+        drawPane.setBackground(Color.WHITE);
+		c.add(rightPanel, BorderLayout.EAST);
+        c.add(drawPane, BorderLayout.CENTER);
+
+        
+        
+        
+        //텍스트박스 내 댓글을 ListPane에 올리기
+        InputMap im = consolePane.getInputMap(JComponent.WHEN_FOCUSED);
+        ActionMap am = consolePane.getActionMap();
+
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "enterPressed");
+
+        am.put("enterPressed", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String text = consolePane.getText();
+                model.addElement("User: " + text);
+                consolePane.setText("");
+            }
+        });
+        
 		pack(); 
 		setSize(1024, 712);
         setVisible(true);
@@ -180,16 +219,17 @@ public class EditorEx extends JFrame {
     		
     	   	addMouseListener(new MouseAdapter(){
         		public void mousePressed(MouseEvent e) {
-        			start.x = e.getX();
-        			start.y = e.getY();
+//        			start.x = e.getX();
+//        			start.y = e.getY();
 				
 					currentSpline = new Vector<>();
     				MyPoint p = new MyPoint(e.getX(), e.getY());
-    				currentSpline.add(p);
-    				splineList.add(currentSpline);
     				erasedSplineList.clear(); //새 Spline 작성 시 Redo 초기화(중첩 방지)
         	   		p.setColor(EditorEx.this.currentColor);
         	   		p.setWidth(EditorEx.this.penWidth);
+    				currentSpline.add(p);
+    				splineList.add(currentSpline);
+    				repaint();
         		}
         		
     	   	});	
@@ -203,6 +243,14 @@ public class EditorEx extends JFrame {
     				repaint();
     	   		}
     	   	});
+    	   	
+//    	   	addKeyListener(new KeyAdapter() {
+//    	   		public void keyPressed(KeyEvent e) {
+//    	   			if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+//    	   				System.out.println("엔터");
+//    	   			}
+//    	   		}
+//    	   	});
     	}
     	
     	public void paintComponent(Graphics g) {
@@ -210,6 +258,14 @@ public class EditorEx extends JFrame {
 			
 			for(Vector<MyPoint> spline : splineList) {
 				Graphics2D g2 = (Graphics2D) g;
+				
+				if (spline.size() == 1) {
+					g.setColor(spline.get(0).pointColor);
+		    		g2.setStroke(new BasicStroke(spline.get(0).width, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+					g.drawLine(spline.get(0).x, spline.get(0).y, spline.get(0).x, spline.get(0).y);
+					
+					continue;
+				}
 				
 				for (int i = 1; i < spline.size(); i++) {
 					MyPoint temp1 = spline.get(i-1);
